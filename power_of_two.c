d946bebf1dc1ec5df92ebb7567ee7dea27cabc3a
Irfan Karim <irfankarimhaji1122@gmail.com>
2024-12-10 20:06:17 +0500

    Power of two

--- power_of_two.c
+++ power_of_two.c
@@ -1,4 +1,4 @@
-/*
+/*---->
 Given an integer n, return true if it is a power of two. Otherwise, return false.
 An integer n is a power of two, if there exists an integer x such that n == 2x. 
 
@@ -23,7 +23,7 @@ bool isPowerOfTwo(int n) {
       while (n>1) { 
         if (n%2 != 0) 
         return false; 
-        n /= 2; 
+        n/=2; 
         } 
         return true;
 }
